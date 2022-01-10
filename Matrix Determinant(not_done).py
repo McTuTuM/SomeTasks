3b3f0b394074f9eda85e57f899846b4230e4204d
McTuTuM <marchenko.k@interset.su>
2022-01-10 13:19:45 +0300

    1

--- Matrix Determinant(not_done).py
+++ Matrix Determinant(not_done).py
@@ -1,13 +1,12 @@
 # Write a function that accepts a square matrix (N x N 2D array) and returns the determinant of the matrix.
 def determinant(matrix):
     temp = []
-    if len(matrix) > 1:
+    if len(matrix) > 2:
         for i in range(len(matrix) - 1):
             temp.append([matrix[0][i], matrix[0][i + 1]])
-        determinant(matrix)
+        determinant(temp)
     else:
-        matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    return 
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
 
 m1 = [[2,5,3], [1,-2,-1], [1, 3, 4]]
